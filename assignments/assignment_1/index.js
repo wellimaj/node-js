d7ef20e44e624e375834f3958d3b01d24ec222bf
wellimaj <adityajoshi3313@gmail.com>
2021-12-27 14:22:02 +0530

    assignment-1

--- assignments/assignment_1/index.js
+++ assignments/assignment_1/index.js
@@ -1,17 +1,29 @@
 function getNameFromCommandLine() {
-    // Write you code here, name should be taken as args in process.argv
+  const process = require("process");
+  // Write you code here, name should be taken as args in process.argv
+  const args = process.argv;
+  return args[args.length - 1];
 }
 
 function getNameFromEnv() {
-    // Write your code here
+  // Write your code here
+  process.env.name = "Yash";
+  return process.env.name;
 }
 
 function getNameFromReadLine() {
-    // Write your code here
+  var readline = require("readline");
+
+  var rl = readline.createInterface(process.stdin, process.stdout);
+  rl.question("What is your name? ", (name) => {
+    console.log(name);
+  });
+  // Write your code here
 }
+getNameFromEnv();
 
 module.exports = {
-    getNameFromCommandLine,
-    getNameFromEnv,
-    getNameFromReadLine
-}
+  getNameFromCommandLine,
+  getNameFromEnv,
+  getNameFromReadLine,
+};
